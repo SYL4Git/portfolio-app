5c0a2ece7853fea906ad92ff8f7119c18c65b1bf
SYL4Git <seungyeollee4@gmail.com>
2024-02-27 14:50:35 +0900

    240227 1450 @class

--- src/styledComponent/StyledHome.js
+++ src/styledComponent/StyledHome.js
@@ -5,28 +5,77 @@ export const StyledHome = styled.div`
 	width: 100%;
 	margin: 0 auto;
 `;
+
 export const StyledAbout = styled.div`
-	width: 1200px;
-	margin: 160px auto;
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+	min-width: 360px;
+	margin: 160px auto;
 
 	.bannerImg {
 		width: 50%;
 		height: auto;
-		margin-bottom: 50px;
-	}
-	.bannerImg img {
-		width: 100%;
-		height: auto;
+		margin: 0 auto 50px;
+
+		img {
+			width: 100%;
+			height: auto;
+		}
 	}
 	.catchPhrase {
-		width: 300px;
-		margin: 50px auto;
+		width: 50%;
+		margin: 0px auto;
 		.wia {
+			display: inline-block;
+			text-align: center;
 			margin-top: 80px;
 			margin-bottom: 100px;
+
 			p {
 				margin-bottom: 20px;
+			}
+			.frst {
+				// width: 200px;
+				font-size: 26px;
+				line-height: 30px;
+				font-weight: 600;
+			}
+			.scnd {
+				font-size: 14px;
+			}
+		}
+		.creed {
+			display: inline-block;
+			text-align: center;
+			.frst {
+				// width: 200px;
+				margin-bottom: 20px;
+				font-size: 26px;
+				line-height: 30px;
+				font-weight: 600;
+			}
+			.scnd,
+			.thrd,
+			.frth {
+				margin-bottom: 10px;
+				font-size: 14px;
+			}
+		}
+	}
+
+	@media (min-width: 768px) {
+		display: flex;
+		flex-direction: row;
+		align-items: stretch;
+
+		.bannerImg {
+			margin: 0 auto;
+		}
+		.catchPhrase {
+			margin: 0px auto;
+			.wia {
+				margin-top: 0;
 			}
 			.frst {
 				font-size: 40px;
@@ -36,19 +85,7 @@ export const StyledAbout = styled.div`
 				font-size: 20px;
 			}
 		}
-		.creed {
-			.frst {
-				margin-bottom: 20px;
-				font-size: 40px;
-				line-height: 40px;
-			}
-			.scnd,
-			.thrd,
-			.frth {
-				margin-bottom: 10px;
-				font-size: 18px;
-				line-height: 20px;
-			}
-		}
+	}
+	@media (min-width: 1200px) {
 	}
 `;
